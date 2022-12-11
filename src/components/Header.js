eba3e7320ac74b7eab409d8df738de8f936be28e
unknown <marcinoles87@gmail.com>
2022-12-11 20:45:42 +0100

    add css and structure

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import  './Header.css'
+
+
+class Header extends React.Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>Header</div>
+        );
+    }
+}
+ 
+export default Header;
---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Menu extends React.Component {
+
+
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                Menu
+            </div>
+        );
+    }
+}
+ 
+export default Menu;
