ee0335fe39ebf0848c6f07d82e4eb14db51918f5
unknown <marcinoles87@gmail.com>
2022-12-12 20:33:18 +0100

    work with structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,30 @@ class Header extends React.Component {
     state = {  } 
     render() { 
         return (
-            <div>Header</div>
+            <div className='container'>
+                <div className='header-logo'>
+                    <a href='/'>
+                        <img src='../img/logo.png' alt='logo'></img>
+                    </a>
+
+                <nav className='header-nav-right'>
+                    <ul>Menu
+                        <li>About Me</li>
+                        <li>Products</li>
+                        <li>Contact</li>
+                    </ul>
+
+                </nav>
+
+                <div className='header-main'>
+                    <h1>Hello and Welcome !</h1>
+                    <h4> This website is my second project , I hope se you enyoi it !
+                        Search , Add and teaste with flavour with smile 
+                    </h4>
+                </div>
+
+                </div>
+            </div>
         );
     }
 }
