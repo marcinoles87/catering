2660392f429448d8f8dfe5facbf5024da7142f3d
unknown <marcinoles87@gmail.com>
2022-12-16 17:38:36 +0100

    add section 2

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -11,9 +11,15 @@ class Menu extends React.Component {
             <div className='menu-main'>
                 
                 
-                <div className='a'>1</div>
-                <div className='b'>2</div>
-                <div className='c'>3</div>
+                <div className='a'>1
+                    <img src='logo.png' alt='a-logo'></img>
+                </div>
+                
+                <div className='b'>2
+                </div>
+                
+                <div className='c'>3
+                </div>
                 
             </div>
         );
