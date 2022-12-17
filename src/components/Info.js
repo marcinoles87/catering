809f6db667d1e9e34497cf197da3248ea2042412
unknown <marcinoles87@gmail.com>
2022-12-17 17:54:09 +0100

    new section add

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,9 +6,15 @@ class Info extends React.Component {
     state = {  } 
     render() { 
         return (
-            <div>
-                <div className="main-info">
+            <div className="info-wrapper">
+                <div className="info-main">
                     <h1>Info for our Customer !</h1>
+                    <p>Tease people you know well because you are more 
+                        likely to be in tune with what is sensitive to them. 
+                        Certain topics should be off limits including but not limited
+                         to politics, religion, race, gender, cultural belie</p>
+
+                        <button> Click to see more</button>
                 </div>
             </div>
         );
