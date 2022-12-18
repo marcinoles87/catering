6ab7d3c373ddad47b21db21c91ab0f0096d40a02
unknown <marcinoles87@gmail.com>
2022-12-18 10:05:49 +0100

    new section

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,12 +7,15 @@ class Gallery extends React.Component {
     render() { 
         return (
             <div className="gallery-wrapper">
+                <h1 className="gallery-head"> Gallery of our Products</h1>
+                <div className="gallery">
                 <div className="gallery-card">1</div>
                 <div className="gallery-card">2</div>
                 <div className="gallery-card">3</div>
                 <div className="gallery-card">4</div>
                 <div className="gallery-card">5</div>
                 <div className="gallery-card">6</div>
+                </div>
             </div>
         );
     }
