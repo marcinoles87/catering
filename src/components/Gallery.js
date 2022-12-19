49c803b1183f076614a813c09f676af3596fe8d9
unknown <marcinoles87@gmail.com>
2022-12-19 20:26:00 +0100

    gallery card add

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import '../components/gallery.css'
-import image from '../img/logo.png'
+import image from '../img/salmon.jpg'
 
 class Gallery extends React.Component {
     state = {  } 
@@ -10,12 +10,13 @@ class Gallery extends React.Component {
             <div className="gallery-wrapper">
                 <h1 className="gallery-head"> Gallery of our Products</h1>
                 <div className="gallery">
-                <div className="gallery-card">1</div>
-                <div className="gallery-card">2</div>
-                <div className="gallery-card">3</div>
-                <div className="gallery-card">4</div>
-                <div className="gallery-card">5</div>
-                <div className="gallery-card">6</div>
+                <div className="gallery-card"><img src={image} alt='salmon' className="gallery-img"></img></div>
+                <div className="gallery-card"><img src={image} alt='salmon' className="gallery-img"></img></div>
+                <div className="gallery-card"><img src={image} alt='salmon' className="gallery-img"></img></div>
+                <div className="gallery-card"><img src={image} alt='salmon' className="gallery-img"></img></div>
+                <div className="gallery-card"><img src={image} alt='salmon' className="gallery-img"></img></div>
+                <div className="gallery-card"><img src={image} alt='salmon' className="gallery-img"></img></div>
+               
                 </div>
             </div>
         );
