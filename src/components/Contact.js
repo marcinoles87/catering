128ce7e7a30961dfb69fd6810136b90a280995d2
unknown <marcinoles87@gmail.com>
2022-12-19 20:30:54 +0100

    add Contact

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class Contact extends React.Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className='contact-wrapper'>
+
+
+            </div>
+        );
+    }
+}
+ 
+export default Contact;
