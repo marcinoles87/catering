b389e514afc10493dd6ea93a2c3e30e328a7acdf
unknown <marcinoles87@gmail.com>
2023-01-04 16:43:16 +0100

    footer add

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -155,7 +155,7 @@ class Contact extends React.Component {
                
                    <h1>Check Us On and follow:</h1>
 
-                    <img src={logo1} alt='tripadvisor'></img>
+                    <img src={logo1} alt='tripadvisor' ></img>
                     <img src={logo2} alt='facebooklogo'></img>
                     <img src={logo3} alt='instagram'></img>
 
