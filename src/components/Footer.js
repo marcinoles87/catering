180bdbbbf58349edca8856302b97a33e1195ac9b
unknown <marcinoles87@gmail.com>
2023-01-04 16:36:09 +0100

    footer add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,12 @@ import footer from '../components/footer.css'
 
 const Footer = () => {
     return (  
-        <div>
-            <h1>Footer 2023</h1>
+        
             <div className="newsletter">
                 <h1>You place to follow us in Time :</h1>
                 <input placeholder="input your email"></input>
             </div>
-        </div>
+        
     );
 }
  
