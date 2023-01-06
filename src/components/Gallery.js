f81450e3ab3f603fab03aecb5c3f8e8ee1810428
unknown <marcinoles87@gmail.com>
2023-01-06 08:19:15 +0100

    gallery modal add

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,13 +4,25 @@ import '../components/gallery.css'
 import image from '../img/salmon.jpg'
 
 class Gallery extends React.Component {
+
+
     state = {  } 
+
+    handleOnClick = () => {
+
+        console.log('dziala')
+        const imageBig = document.getElementsByClassName('gallery-img')
+        console.log(imageBig)
+        
+        
+
+    }
     render() { 
         return (
             <div className="gallery-wrapper">
                 <h1 className="gallery-head"> Gallery of our Products</h1>
                 <div className="gallery">
-                <div className="gallery-card"><img src={image} alt='salmon' className="gallery-img"></img></div>
+                <div className="gallery-card"><img src={image} onClick={this.handleOnClick} alt='salmon' className="gallery-img"></img></div>
                 <div className="gallery-card"><img src={image} alt='salmon' className="gallery-img"></img></div>
                 <div className="gallery-card"><img src={image} alt='salmon' className="gallery-img"></img></div>
                 <div className="gallery-card"><img src={image} alt='salmon' className="gallery-img"></img></div>
