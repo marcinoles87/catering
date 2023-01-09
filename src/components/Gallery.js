236a92089e0df5f7c01a73ccba45b39cd63b2db2
unknown <marcinoles87@gmail.com>
2023-01-09 17:10:53 +0100

    work with gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -18,8 +18,8 @@ class Gallery extends React.Component {
     
 
     handleOnClick = (e) => {
-        const n = e.target.value
-        console.log(n)
+    
+        console.log(e)
         const imageBig = document.querySelector('.gallery-img')
         console.log(imageBig)
         imageBig.classList.toggle('imageBig')
@@ -34,9 +34,9 @@ class Gallery extends React.Component {
                 <h1 className="gallery-head"> Gallery of our Products</h1>
                 <div className="gallery">
                 <div className="gallery-card"><img src={image} onClick={this.handleOnClick} alt='salmon' className="gallery-img"></img></div>
-                <div className="gallery-card"><img src={image2} onClick={this.handleOnClick} alt='salmon' className="gallery-img" ></img></div>
-                <div className="gallery-card"><img src={image3} onClick={this.handleOnClick} alt='salmon' className="gallery-img"></img></div>
-                <div className="gallery-card"><img src={image4} onClick={this.handleOnClick} alt='salmon' className="gallery-img"></img></div>
+                <div className="gallery-card"><img src={image2} onClick={this.handleOnClick} alt='salad' className="gallery-img" ></img></div>
+                <div className="gallery-card"><img src={image3} onClick={this.handleOnClick} alt='dinner' className="gallery-img"></img></div>
+                <div className="gallery-card"><img src={image4} onClick={this.handleOnClick} alt='breakfast' className="gallery-img"></img></div>
                 <div className="gallery-card"><img src={image} onClick={this.handleOnClick} alt='salmon' className="gallery-img"></img></div>
                 <div className="gallery-card"><img src={image} onClick={this.handleOnClick} alt='salmon' className="gallery-img"></img></div>
                
