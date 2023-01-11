101f9facc01d6334d520c9b9b22040b70ddf1d70
unknown <marcinoles87@gmail.com>
2023-01-11 16:50:21 +0100

    work with gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,23 +8,7 @@ import image3 from '../img/dinner.jpg'
 import image4 from '../img/salad.jpg'
 
 
-const Gal = () => {
 
-    const showNam = true
-    const names = ['kev' , 'lev' , 'van']
-    const images  = names.map( name => {
-        <li key={name}>{name}</li>
-    })
-
-
-    return (
-
-
-        <div>
-            {showNam ?<li>{images}</li> : 'brak wyswietlenia'}
-            </div>
-    )
-}
 
 
 class Gallery extends React.Component {
@@ -83,6 +67,12 @@ class Gallery extends React.Component {
 
     }
     render() { 
+
+        const showNam = true
+        const names = ['kev' , 'lev' , 'van']
+        const images  = names.map( name => {
+            <li key={name}>{name}</li>
+        })
     
 
         return (
@@ -95,7 +85,7 @@ class Gallery extends React.Component {
                 <div className="gallery-card"><img src={image4} onClick={this.handleOnClick} alt='breakfast' className="gallery-img"></img></div>
                 <div className="gallery-card"><img src={image} onClick={this.handleOnClick} alt='salmon' className="gallery-img"></img></div>
                 <div className="gallery-card"><img src={image} onClick={this.handleOnClick} alt='salmon' className="gallery-img"></img></div>
-                <Gal />
+                <p>{images}</p>
              
                 </div>
             </div>
